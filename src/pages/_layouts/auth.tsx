cdb52a1fd14b6b257957edc3e4157e7d1b9e8c0a
José Airton Neto <jose.neto.airton@gmail.com>
2025-07-27 13:06:41 -0300

    Setup do projeto

---
+++ src/pages/_layouts/auth.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+
+export function AuthLayout() {
+  return (
+    <>
+      <h1>AuthLayout</h1>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
